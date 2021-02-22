624e4a5b142476b898a3f584a7b2740b2095b58e
Om Sharan Salafia <omsharan.salafia@gmail.com>
2021-02-22 12:39:47 +0100

    ex1 code commented

--- lesson1/ex1.py
+++ lesson1/ex1.py
@@ -1,3 +1,6 @@
+# Solution to Exercise 1
+
+# define n_th root function
 def nth_root(x,n):
     """
     Compute the n-th root of x.
@@ -20,6 +23,8 @@ def nth_root(x,n):
     
     return x**(1./n)
 
+
+# create lists containing the first 5 odd numbers and their sums
 odds = []
 sums = []
 
@@ -38,7 +43,12 @@ for i in range(len(odds)):
 
 print(sums)
 
+# compute the square roots of the sums of the first 5 odd numbers
 roots = [nth_root(x,2) for x in sums]
 
 print(roots)
+
+# demonstrate the internal checks in the function
+nth_root(-2,2)
+nth_root(4,2.0)
     
